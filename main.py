1953375d7b0b926dec7011cc8aeb30bedb910aa3
karolinaryba <karolina.ryba@cloudreach.com>
2021-11-11 17:51:55 +0000

    app example

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 #!/Users/karolina.ryba/.pyenv/shims/python
-
+import json
 from flask import Flask, request, Response
 
 app = Flask(__name__)
+
+movie_db = {
+    "1" : {
+        "name" : "Stargate", 
+        "release_date" : "1994"},
+    "2" : {
+        "name" : "Sunshine", 
+        "release_date" : "2007"},
+    "3" : {
+        "name" : "The Holiday",
+        "release_date" : "2006"}
+}
 
 @app.route("/")
 def hello():
     return "Hello world"
 
-@app.route("/hello")
-def world():
-    return "<h1>Hello World!!!!!</h1>"
+@app.route("/movies")
+def movies():
+    return json.dumps(movie_db)
+
+#read
+@app.route("/movie/<movie_id>")
+def get_movie(movie_id):
+    return json.dumps(movie_db[movie_id])
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1')
+
+    #create(post), read(get), update(put), delete(get)
